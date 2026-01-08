44b7e740c3c153eaae1ab07a674e47dcc7513c64
MoisesLucasGA <mlucas.gondim@gmail.com>
2026-01-08 18:04:49 -0300

    feat: question #222

---
+++ 222-countNodes.ts
@@ -0,0 +1,40 @@
+/**
+ * 222. Count Complete Tree Nodes
+Easy
+Topics
+premium lock iconCompanies
+
+Given the root of a complete binary tree, return the number of the nodes in the tree.
+
+According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+Design an algorithm that runs in less than O(n) time complexity.
+*/
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function countNodes(root: TreeNode | null): number {
+  if (!root) {
+    return 0;
+  }
+
+  return 1 + countNodes(root.left) + countNodes(root.right);
+}
+
+const root = new TreeNode(
+  1,
+  new TreeNode(2, new TreeNode(4), new TreeNode(5)),
+  new TreeNode(3, new TreeNode(6), new TreeNode(7))
+);
+
+console.log(countNodes(root));
